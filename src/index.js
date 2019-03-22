0597feefcea890c4335057d4944907022b8cd1d3
Mohamed-Elwan <mesee1@gmail.com>
2019-03-22 22:41:00 +0200

    using single div instead of two

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import Counter from './components/counter'
 import "./styles.css";
 
 const App = () => {
-  return <div></div>;
+    return <div>
+        <Example/>
+        <div> --------------------</div>
+        <Counter/>
+    </div>;
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Example />, rootElement);
-
-const counterElement = document.getElementById("counter");
-ReactDOM.render( <Counter / > , counterElement);
+ReactDOM.render(<App/>, rootElement);
